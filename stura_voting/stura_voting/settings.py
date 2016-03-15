09d05c948df813a4457f20bc7ecf2f529de4e8ac
Fabian Wenzelmann <wenzelmf@informatik.uni-freiburg.de>
2016-03-15 16:57:49 +0100

    initial models, with basic poll structore

--- stura_voting/stura_voting/settings.py
+++ stura_voting/stura_voting/settings.py
@@ -53,6 +53,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'votes',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- stura_voting/votes/models.py
+++ stura_voting/votes/models.py
@@ -21,5 +21,82 @@
 # SOFTWARE.
 
 from django.db import models
+from django.utils.translation import ugettext_lazy as _, ungettext_lazy
+import django.utils
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+
+from decimal import Decimal
+import uuid
 
 # Create your models here.
+
+
+class VotingTerm(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    pub_date = models.DateTimeField(
+        _('Erstellungszeitpunkt'),
+        default=django.utils.timezone.now)
+    name = models.CharField(
+        _('Name'),
+        help_text=_('Name der Abstimmungsperiode'),
+        max_length=40,
+        unique=True)
+
+
+class VotingWeightGroup(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    term = models.ForeignKey(
+        VotingTerm,
+        on_delete=models.CASCADE,
+        verbose_name=_('Abstimmungszeitraum'),
+        help_text='Abstimmungszeitraum dem diese Gewichtung zugeordnet wird')
+
+
+class VotingWeight(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(
+        _('Name'),
+        help_text=_('Name der abstimmenden Gruppe'),
+        max_length=75)
+    weight = models.PositiveSmallIntegerField(
+        verbose_name=_('Gewichtung'),
+        help_text=_('Die Abstimmungsgewichtung der Gruppe'))
+    group = models.ForeignKey(
+        VotingWeightGroup,
+        on_delete=models.CASCADE,
+        verbose_name=_('Gruppe'),
+        help_text=_('Gruppe zu der diese Eintragung gehört'))
+
+    class Meta:
+        unique_together = ('group', 'name')
+
+
+class VotingDay(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    date = models.DateField(
+        verbose_name=_('Datum'),
+        help_text='Datum an dem die Abstimmungen durchgeführt werden.')
+    weights = models.ForeignKey(
+        VotingWeightGroup,
+        on_delete=models.CASCADE,
+        verbose_name=_('Gewichtungsgruppe'),
+        help_text=_('Gewichtungsgruppe welche zur Auszählung verwendet wird'))
+
+
+class GeneralPoll(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    percent_required = models.DecimalField(
+        _('Prozent'),
+        help_text=_(
+            'Prozent die nötig sind, damit die Abstimmung als angenommen gilt'),
+        max_digits=4,
+        decimal_places=2,
+        default=Decimal('50.00'),
+        validators=[MinValueValidator(Decimal('0.00')),
+                    MaxValueValidator(Decimal('100.00'))])
+    all_votes = models.BooleanField(
+        _('Alle Stimmen?'),
+        help_text=_(
+            'Wenn aktiviert werden ALLE Stimmen gezählt, auch jene die nicht mit abgestimmt haben!'),
+        default=False)
